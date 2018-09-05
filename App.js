fec340c9805cc02a3df036ac8cbd0f358e35444e
Rafa Gallego <rgdmax@gmail.com>
2018-09-05 18:29:50 +0200

    First test

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// const redditURL = 'https://api.reddit.com/r/pics/new.json';
+
 export default class App extends React.Component {
+
+  // getRedditInfo() {
+  //   return fetch(redditURL, {
+  //   method: 'GET',
+  //   headers: {
+  //     Accept: 'application/json',         
+  //     'Content-Type': 'multipart/form-data'
+  //   },
+  //   body: formdata,
+  // })       
+  // .then(response => response.json())
+  // .then(data => {         
+  //   this.setState({
+  //     isLoading: false,
+  //     quote: data
+  //   })
+  // })    
+  // .catch(error => console.error(error)) 
+  // }
+
+  // componentDidMount() {     
+  //   this.getRedditInfo();
+  // }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Shake your phone to open the developer menu. HOLA</Text>
       </View>
     );
   }
