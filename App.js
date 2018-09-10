3c1671c6bc52a42bcc1c9b4d4f89ced7cfda91ea
Rafa Gallego <rgdmax@gmail.com>
2018-09-10 22:59:24 +0200

    First version working with promises and async-await

--- App.js
+++ App.js
@@ -1,41 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-// const redditURL = 'https://api.reddit.com/r/pics/new.json';
+const redditURL = 'https://api.reddit.com/r/pics/new.json';
 
 export default class App extends React.Component {
 
-  // getRedditInfo() {
-  //   return fetch(redditURL, {
-  //   method: 'GET',
-  //   headers: {
-  //     Accept: 'application/json',         
-  //     'Content-Type': 'multipart/form-data'
-  //   },
-  //   body: formdata,
-  // })       
-  // .then(response => response.json())
-  // .then(data => {         
-  //   this.setState({
-  //     isLoading: false,
-  //     quote: data
-  //   })
-  // })    
-  // .catch(error => console.error(error)) 
-  // }
+  state = {
+    loading: true,
+    dataSource: []
+  }
 
-  // componentDidMount() {     
-  //   this.getRedditInfo();
-  // }
+  async componentDidMount() {
+    await this.makeRemoteRequest();
+  }
+
+  async fetchData(url) {
+    const result = await fetch(url);        
+    return await result.json();
+  }      
+
+  async makeRemoteRequest() {     
+    try {      
+      const responseData = await this.fetchData(redditURL)
+      
+      this.setState({
+        loading: false,
+        dataSource: responseData
+      })
+
+
+    } catch(error) {      
+      this.setState({ error, loading: false });
+    }
+
+    // fetch(redditURL)
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     this.setState({
+    //       loading: false,
+    //       dataSource: res          
+    //     });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ error, loading: false });
+    //   });
+  };
+
+  renderLoadingView() {
+    return(
+      <View>
+        <Text>Cargando reddits... </Text>
+      </View>
+    )
+  }
 
   render() {
+    const { loading, dataSource } = this.state;    
+
+    if (loading) {            
+      return this.renderLoadingView(); 
+    }           
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu. HOLA</Text>
-      </View>
-    );
+    <View style={styles.container}>
+      <Text>Soy el test de Reddit</Text>      
+      <FlatList
+        data = { dataSource.data.children }
+        renderItem={({item}) => <Text>{item.data.title}</Text>}
+      />
+    </View>
+    );   
   }
 }
 
